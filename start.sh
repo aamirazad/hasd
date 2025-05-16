b74cc7e8b76c7d2aeae629b02f46a0340f30a4c9
Aamir Azad <aamirmazad@gmail.com>
2025-05-16 18:25:12 -0400

    delete everthing, astro skeleten

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tmux new-session -d -s calos \
-	'templ generate --watch --proxy=http://localhost:8080 --proxyport=3000 -path ./src/templates --open-browser=false -v' \; \
-	split-window -v 'tailwindcss -i ./src/static/input.css -o ./src/static/styles.css --watch' \; \
-	split-window -h 'air' \; \
-	select-layout tiled \; \
-	attach
