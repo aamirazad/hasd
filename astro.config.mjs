1c1c5b762bf9ca6b718324bcf2bf7f38526d964b
Aamir Azad <82281117+aamirazad@users.noreply.github.com>
2025-06-26 17:16:26 +0000

    I folded, 2 looks so much better for html

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,24 @@
 import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
-import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
-    site: "https://hasd.aamira.me",
+  site: "https://hasd.aamira.me",
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-    env: {
-        schema: {
-            POCKETBASE_SUPER_EMAIL: envField.string({
-                context: "server",
-                access: "secret",
-            }),
-            POCKETBASE__SUPER_PASSWORD: envField.string({
-                context: "server",
-                access: "secret",
-            }),
-        },
+  env: {
+    schema: {
+      POCKETBASE_SUPER_EMAIL: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      POCKETBASE_SUPER_PASSWORD: envField.string({
+        context: "server",
+        access: "secret",
+      }),
     },
-    integrations: [alpinejs()],
+  },
 });
